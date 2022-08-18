f24c9b63c28ec192d86ff0b21b672350902d8a49
«Svetlana» <desko.nn@gmail.com>
2022-08-18 23:02:59 +0300

    Выполнили задачу 10

---
+++ dz2.10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Задача 10 ");
+Console.Write("Введи трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(a);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+
